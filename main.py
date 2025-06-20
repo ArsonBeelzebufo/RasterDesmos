c7ef72292f4f396337afb0e78641049fcfee0ee4
cassiopeia3 <warrenw3g@gmail.com>
2025-06-20 13:01:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import dotenv
 import os
 
+def polygonize(*pixel): #x,y,rgba
+    x=pixel[0]
+    y=-pixel[1]
+    c=pixel[2]
+    return f"\\operatorname{{polygon}}\\left(\\left({x},{y}\\right),\\left({x+1},{y}\\right),\\left({x+1},{y-1}\\right),\\left({x},{y-1}\\right)\\right)",f"\\operatorname{{rgb}}\\left({int(c[:2],16)},{int(c[2:4],16)},{int(c[4:6],16)}\\right)",f"{int(c[6:],16)}/255"
+def equatize(colorArray):
+    eqs="["
+    cEq="C=["
+    oEq="O=["
+    for y in range(len(colorArray)):
+        for x in range(len(colorArray[y])):
+            temp=polygonize(x,y,colorArray[y][x])
+            eqs+=temp[0]+','
+            cEq+=temp[1]+','
+            oEq+=temp[2]+','
+    eqs=eqs[:-1]+']'
+    cEq=cEq[:-1]+']'
+    oEq=oEq[:-1]+']'
+    return eqs+'\n'+cEq+'\n'+oEq
+
 dotenv.load_dotenv()
 filepath=os.getenv("BMPPath")
 with open(filepath,'rb') as file:
     content=file.read()
     fileHead=content[:14]
-    
